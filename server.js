75337a7174c437ee47fea927a06f2411730b7d51
InnoSphear <innosphear@gmail.com>
2026-02-25 19:20:24 +0530

    Update CORS configuration in server.js
    Simplified CORS configuration to allow all origins.

--- server.js
+++ server.js
@@ -11,10 +11,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
 const app = express()
-app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000', 'http://127.0.0.1:5173'],
-  credentials: true
-}))
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
